2a84c3eab3a48cb3803aa900af3548daf0598c1f
wiktor <wiktor.wojciechowski@korbank.pl>
2020-07-30 12:02:36 +0200

    add tests for parse command

---
+++ teryt/tests/test_command.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+from django.core import management
+from django.test import TestCase
+
+from teryt.models import RodzajMiejscowosci, JednostkaAdministracyjna, Miejscowosc, Ulica
+
+
+class TestParseCommand(TestCase):
+
+    files = [
+        "teryt/tests/assets/SIMC_Adresowy_2020-07-29.zip",
+        "teryt/tests/assets/TERC_Adresowy_2020-07-29.zip",
+        "teryt/tests/assets/ULIC_Adresowy_2020-07-29.zip",
+        "teryt/tests/assets/WMRODZ_2020-07-29.zip",
+    ]
+
+    def test_command(self):
+        management.call_command('teryt_parse', *self.files)
+        self.assertTrue(RodzajMiejscowosci.objects.count())
+        self.assertTrue(JednostkaAdministracyjna.objects.count())
+        self.assertTrue(Miejscowosc.objects.count())
+        self.assertTrue(Ulica.objects.count())
